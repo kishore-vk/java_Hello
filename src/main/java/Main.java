ff41f7e1dd4477421941e133bb6a88f8f461f37c
sinohore <kishorramanan@gmail.com>
2015-10-09 08:45:07 +0000

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 package localdomain.localhost.tutorial;
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world! How are you? Live Big, Live Free");
+    System.out.println("Greeting! Have a good day");
   }
 }
