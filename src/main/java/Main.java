f20edc79b9f72bcfd051b814d610710e53f4a4c5
sinohore <kishorramanan@gmail.com>
2015-10-11 06:29:23 +0000

    output changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ package localdomain.localhost.tutorial;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Poda Goyya gokul!");
+    System.out.println("Good day");
      }
 }
