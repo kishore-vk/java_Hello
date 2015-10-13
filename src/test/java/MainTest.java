832c3104c17fdfc6d17b40fdb1f0f45bd0702093
sinohore <kishorramanan@gmail.com>
2015-10-13 11:50:44 +0000

    adding test func

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,18 @@
+package localdomain.localhost.tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		
+		
+	 assertEquals("Output should be 11", 11, StringPrg.add());
+		
+		
+	}
+
+}
