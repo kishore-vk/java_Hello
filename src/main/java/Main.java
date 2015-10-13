dd3552c4aa90263dbf21bdb04d97a25099b51dd3
sinohore <kishorramanan@gmail.com>
2015-10-13 11:09:17 +0000

    Introduced Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
-package localdomain.localhost.tutorial;
-
-
 public class Main {
+    
   public static void main(String[] args) {
-    System.out.println("Good day");
+	  
+    printOne();
+	
+    int b = add();
+	
+	System.out.println(b);
+	
+    }
+     
+     public static void printOne() {
+		 
+		System.out.println("Hello World");
      }
+     
+     public static int add() {
+         int c = 5+6;
+         return (c);
+     }
+     
 }
