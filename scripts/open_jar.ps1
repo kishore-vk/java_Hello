5bcecde73b56ee65e81222b1aa60feec761d0fd2
sinohore <kishorramanan@gmail.com>
2015-10-19 14:04:21 +0000

    scipts

---
+++ scripts/open_jar.ps1
@@ -0,0 +1,4 @@
+java -jar "C:/Users/Administrator/.jenkins/workspace/Hello world/target/java_jar-1.0-SNAPSHOT.jar" > "c:/code/output.txt"
+start "C:/code/output.txt"
+start "C:/Users/Administrator/.jenkins/workspace/Hello world/target/site/cobertura/index.html"
+start "C:/Users/Administrator/.jenkins/workspace/Hello world/target/site/apidocs/index.html"
