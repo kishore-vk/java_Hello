c38923059c442bc2c4495ec6e3bc2a8661327b8e
sinohore <kishorramanan@gmail.com>
2015-10-19 16:17:11 +0000

    Output changes

--- src/main/java/demo/helloworld/Main.java
+++ src/main/java/demo/helloworld/Main.java
@@ -8,18 +8,18 @@ public class Main {
 	public static void main(String[] args) {
 		int a = 10, b = 5, c = 0;
 		System.out.println(printOne() + "\n");
-		/* Invoking addition from Main1 Class */
-		// c = Main1.add(a, b);
-		// System.out.println("Result of Addition is: " + c + "\n");
-		/*Invoking Subtraction from Main2 Class*/
-		// c = Main2.sub(a, b);
-		// System.out.println("Result of Subtraction is: " + c);
+		// /* Invoking addition from Main1 Class */
+		c = Main1.add(a, b);
+		System.out.println("Result of Addition is: " + c + "\n");
+		// /*Invoking Subtraction from Main2 Class*/
+		c = Main2.sub(a, b);
+		System.out.println("Result of Subtraction is: " + c);
 	}
 
 	// HelloWorld Function
 	public static String printOne() {
 		String bs;
-		bs = "Hello! Have a great weekend";
+		bs = "Hello! How are you?";
 		return bs;
 	}
 
