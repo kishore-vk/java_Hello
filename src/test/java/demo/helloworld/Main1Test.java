8bde31ef9e8b8646c7f6e90325214a947895d3e4
sinohore <kishorramanan@gmail.com>
2015-10-20 09:35:32 +0000

    Adding Test

---
+++ src/test/java/demo/helloworld/Main1Test.java
@@ -0,0 +1,13 @@
+package demo.helloworld;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Main1Test extends Main {
+	@Test
+	public void test() {
+		
+		/* Failure Junit test cases */
+		assertEquals("Output should be 30", 64, Main1.add(15, 12)); 
+		}
+}
--- src/test/java/demo/helloworld/Main2Test.java
+++ src/test/java/demo/helloworld/Main2Test.java
@@ -3,12 +3,11 @@ package demo.helloworld;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class MainTest1 extends Main {
+public class Main2Test extends Main {
 	@Test
 	public void test() {
 		
 		/* Failure Junit test cases */
-		assertEquals("Output should be 30", 27, Main1.add(15, 12)); 
-		assertEquals("Output should be 10", -10, Main2.sub(10,20 ));
+		assertEquals("Output should be 10", -87, Main2.sub(10,20 ));
 		}
 }
